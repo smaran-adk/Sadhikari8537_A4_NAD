b478061a027449cbb3451076119b6a6763b1375f
Smaran Adhikari <rememberadk@gmail.com>
2025-04-09 19:14:32 -0400

    added spinner to the code

---
+++ src/posts/static/posts/main.js
@@ -0,0 +1,36 @@
+
+const helloworldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function (response){
+        console.log('success', response.text)
+        helloworldBox.textContent= response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data =response.data
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el  => {
+                postsBox.innerHTML += `
+                ${el.title} -<b>${el.body}</b><br>
+                `
+            });
+        },100)      
+    },
+    error: function(error){
+        console.log(error)
+    }
+})
