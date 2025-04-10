a49fa6d583d81bf73d6105580903c42bbcb2fa4f
Smaran Adhikari <rememberadk@gmail.com>
2025-04-10 01:00:27 -0400

    Add post created and posts can be adde into it and the alert is also added to it

--- src/posts/views.py
+++ src/posts/views.py
@@ -3,32 +3,56 @@ from .models import Post
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from profiles.models import Profile
 
 # Create your views here.
 
 def post_list_and_create(request):
-    qs =Post.objects.all()
-    return render(request, 'posts/main.html', {'qs':qs})
+    form =PostForm(request.POST or None)
+    # qs =Post.objects.all()
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        if form.is_valid():
+            author= Profile.objects.get(user=request.user)
+            instance=form.save(commit=False)
+            instance.author=author
+            instance.save()
+            return JsonResponse ({
+                'title': instance.title,
+                'body':instance.body,
+                'author': instance.author.user.username,
+                'id': instance.id,
+            })
+            return JsonResponse({'msg':'Post created!'})
+
+    context ={
+        'form': form,
+        
+    }
+
+    return render(request, 'posts/main.html', context)
 
 def load_post_data_view(request, num_posts):
-    visible=3
-    upper=num_posts
-    lower =upper -visible
-    size = Post.objects.all().count()
-    
-    qs =Post.objects.all()
-    data=[]
-    for obj in qs:
-        item={
-            'id':obj.id,
-            'title':obj.title,
-            'body':obj.body,
-            'liked': True if request.user in obj.liked.all() else False,
-            'count': obj.like_count,
-            'author':obj.author.user.username
-        }
-        data.append(item)
-    return JsonResponse({'data':data[lower:upper],'size': size})
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        visible=3
+        upper=num_posts
+        lower =upper -visible
+        size = Post.objects.all().count()
+        
+        qs =Post.objects.all()
+        data=[]
+        for obj in qs:
+            item={
+                'id':obj.id,
+                'title':obj.title,
+                'body':obj.body,
+                'liked': True if request.user in obj.liked.all() else False,
+                'count': obj.like_count,
+                'author':obj.author.user.username
+            }
+            data.append(item)
+        return JsonResponse({'data':data[lower:upper],'size': size})
 
 @require_POST
 @login_required
